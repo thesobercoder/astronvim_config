50eb9c5ba7915fabdcee800803ce9311a0fde60a
Soham Dasgupta <soham1.dasgupta@gmail.com>
2023-06-09 18:27:04 +0530

    chore: remove unused variables

--- plugins/toggleterm.lua
+++ plugins/toggleterm.lua
@@ -3,15 +3,11 @@ return {
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
 
-    local is_win32 = vim.fn.has("win32")
-    if (is_win32 == 1) then
-      config.shell = "powershell.exe"
-      config.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy UnRestricted -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-      config.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
-      config.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-      config.shellquote = ""
-      config.shellxquote = ""
+    local is_windows = vim.fn.has("win32") == 1
+    if (is_windows) then
+      config.shell = "powershell.exe /nologo"
     end
+
     return config -- return final config table
   end,
 }
