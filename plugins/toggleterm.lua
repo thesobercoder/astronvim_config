852c18f7ebf17c65457f9d1d6130a8dfceffe988
Soham Dasgupta <soham1.dasgupta@gmail.com>
2023-06-21 18:54:10 +0530

    configs backup: 2023-06-21 18:54:21

--- plugins/toggleterm.lua
+++ plugins/toggleterm.lua
@@ -1,13 +1,13 @@
 return {
   "akinsho/toggleterm.nvim",
-  opts = function(_, config)
+  opts = function(_, opts)
     -- config variable is the default configuration table for the setup function call
 
     local is_windows = vim.fn.has("win32") == 1
     if (is_windows) then
-      config.shell = "powershell.exe /nologo"
+      opts.shell = "powershell.exe /nologo"
     end
 
-    return config -- return final config table
+    return opts -- return final opts table
   end,
 }
