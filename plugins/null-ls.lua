804f41c90dbe31ffec3d949cf2dc4410b33e531a
Soham Dasgupta <soham1.dasgupta@gmail.com>
2023-06-21 18:54:54 +0530

    configs backup: 2023-06-21 18:55:07

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -1,17 +1,17 @@
 return {
   "jose-elias-alvarez/null-ls.nvim",
-  opts = function(_, config)
+  opts = function(_, opts)
     -- config variable is the default configuration table for the setup function call
     -- local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-    config.sources = {
+    opts.sources = {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
     }
-    return config -- return final config table
+    return opts -- return final opts table
   end,
 }
