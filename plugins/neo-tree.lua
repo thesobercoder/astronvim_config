4027cd34481a16b74722778c6c67aeb54de45764
Soham Dasgupta <soham1.dasgupta@gmail.com>
2023-06-21 19:15:26 +0530

    fix: lua table options for neo-tree

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -4,9 +4,9 @@ return {
     -- config variable is the default configuration table for the setup function call
 
     opts.window.width = 50
-    opts.filesystem.filtered_items.hide_dotfiles = false
-    opts.filesystem.filtered_items.hide_gitignored = false
-    opts.filesystem.filtered_items.hide_hidden = false -- only works on Windows for hidden files/directories
+    opts.filesystem.hide_dotfiles = false
+    opts.filesystem.hide_gitignored = false
+    opts.filesystem.hide_hidden = false -- only works on Windows for hidden files/directories
 
     return opts -- return final config table
   end,
